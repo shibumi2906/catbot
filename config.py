3cbabdafcdd03ada7ad7678071b5e16d6acdcedf
shibumi2906 <164049385+shibumi2906@users.noreply.github.com>
2024-07-12 15:02:00 +0300

    +cat

---
+++ config.py
@@ -0,0 +1,7 @@
+API_TOKEN = '6198574331:AAEw1umL2eLuOEw5_EpbjeeamqSxK6ZCT88'
+
+CAT_API_URL = 'https://api.thecatapi.com/v1/images/search'
+CAT_API_KEY = 'live_zhGHS6dXLLFAnu9usBkIkXGM3oOOr38zKL7v3F1IoDqFM8SOvy3lNNBG4zsjMkwN'
+
+NASA_API_URL = 'https://api.nasa.gov/planetary/apod'
+NASA_API_KEY = 'VlzQ4JP9qkQkkWfcFkRvk6evRM452zYp4XokLbaL'
